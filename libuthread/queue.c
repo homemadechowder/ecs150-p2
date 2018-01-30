5fb2b4f0ce46ad24ac310bb507f426fd2e2f69d1
Joseph Torres <jdtorres@ucdavis.edu>
2018-01-30 15:26:54 -0800

    Added queue delete function

--- libuthread/queue.c
+++ libuthread/queue.c
@@ -13,7 +13,7 @@ struct Node* newNode(void *data)
 {
 	struct Node *temp = (struct Node*)malloc(sizeof(struct Node));
 	if (temp == NULL)
-	return temp;
+		return temp;
 	temp->data = data;
 	temp->next = NULL;
 	return temp;
@@ -29,7 +29,7 @@ queue_t queue_create(void)
 {
 	queue_t myQ = (queue_t)malloc(sizeof(struct queue));
 	if (myQ == NULL)
-	return myQ;
+		return myQ;
 	myQ->front = myQ->rear = NULL;
 	myQ->length = 0;
 	return myQ;
@@ -38,13 +38,11 @@ queue_t queue_create(void)
 int queue_destroy(queue_t queue)
 {	
 	if (queue == NULL || queue->front != NULL)
-	{
-	return -1;
-	}
+		return -1;
 	else 
 	{
-	free(queue);
-	return 0;
+		free(queue);
+		return 0;
 	}
 }
 
@@ -52,13 +50,13 @@ int queue_enqueue(queue_t queue, void *data)
 {
 	struct Node *temp = newNode(data);
 	if (temp == NULL || data == NULL || queue == NULL)
-	return -1;
-	else if (queue->front == NULL)
-	queue->front = queue->rear = temp;
+		return -1;
+	else if (queue->rear == NULL)
+		queue->front = queue->rear = temp;
 	else
 	{
-	queue->rear->next = temp;
-	queue->rear = temp;
+		queue->rear->next = temp;
+		queue->rear = temp;
 	}
 	queue->length++;
 	return 0;
@@ -67,19 +65,45 @@ int queue_enqueue(queue_t queue, void *data)
 int queue_dequeue(queue_t queue, void **data)
 {
 	if (data == NULL || queue == NULL || queue->front == NULL)
-	return -1;
+		return -1;
 	data = &queue->front->data;
+	struct Node* temp = queue->front;
 	queue->front = queue->front->next;
+	free(temp);
 	if (queue->front == NULL)
-	queue->rear = NULL;
+		queue->rear = NULL;
 	queue->length--;
 	return 0;
 }
 
 int queue_delete(queue_t queue, void *data)
 {
+	if (data == NULL || queue == NULL)
+		return -1;
+	struct Node* temp = queue->front;
+	struct Node* prev;
+	if (temp != NULL && temp->data == data)
+	{
+		queue->front = temp->next;
+		free(temp);
+		if (queue->front == NULL)
+		        queue->rear = NULL;
+
+	}
+	else
+	{
+		while (temp != NULL && temp->data != data)
+		{
+			prev = temp;
+			temp = temp->next;
+		}
+		if (temp == NULL)
+			return -1;
+		prev->next = temp->next; 
+		free(temp);
+	}
+	queue->length--; 
 	return 0;
-	/* TODO Phase 1 */
 }
 
 int queue_iterate(queue_t queue, queue_func_t func, void *arg, void **data)
